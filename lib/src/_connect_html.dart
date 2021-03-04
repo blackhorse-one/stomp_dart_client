6a598d8051996b385152c685982ccb9834778feb
Tobias Schwackenhofer <ts@nextgen-social.com>
2021-03-04 15:27:41 +0100

    Get rid of dynamic future

--- lib/src/_connect_html.dart
+++ lib/src/_connect_html.dart
@@ -7,7 +7,7 @@ import 'package:web_socket_channel/web_socket_channel.dart';
 
 Future<WebSocketChannel> connect(StompConfig config) async {
   final webSocket = WebSocket(config.url)..binaryType = BinaryType.list.value;
-  Future onOpenEvent = webSocket.onOpen.first;
+  var onOpenEvent = webSocket.onOpen.first;
   if (config.connectionTimeout.inMilliseconds > 0) {
     onOpenEvent = onOpenEvent.timeout(config.connectionTimeout);
   }
