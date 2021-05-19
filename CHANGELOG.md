f9ce98df6396269c5eea7319e7c7a9bddecb058c
Tobias Kammerer <tobias@blackhorse-one.com>
2021-05-19 22:24:46 +0200

    Fixed onWebSocketError for web (and 0.4.2)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.4.2
+ - Fixed `onWebSocketError` callback for Web
+ - Reworked HTML connect API.
+
 ## 0.4.1
  - Fixed heartbeat formatting
 
--- lib/src/_connect_html.dart
+++ lib/src/_connect_html.dart
@@ -5,12 +5,19 @@ import 'package:stomp_dart_client/stomp_config.dart';
 import 'package:web_socket_channel/html.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
-Future<WebSocketChannel> connect(StompConfig config) async {
+Future<WebSocketChannel> connect(StompConfig config) {
+  final completer = Completer<HtmlWebSocketChannel>();
   final webSocket = WebSocket(config.url)..binaryType = BinaryType.list.value;
-  var onOpenEvent = webSocket.onOpen.first;
+  webSocket.onOpen.first.then((value) {
+    completer.complete(HtmlWebSocketChannel(webSocket));
+  });
+  webSocket.onError.first.then((err) {
+    completer.completeError(WebSocketChannelException.from(err));
+  });
+
   if (config.connectionTimeout.inMilliseconds > 0) {
-    onOpenEvent = onOpenEvent.timeout(config.connectionTimeout);
+    return completer.future.timeout(config.connectionTimeout);
   }
-  await onOpenEvent;
-  return HtmlWebSocketChannel(webSocket);
+
+  return completer.future;
 }
